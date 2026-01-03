7976d63141901a55c002bd62853ccd55696cc593
barrax63 <noah.nowak@icloud.com>
2026-01-03 01:39:01 +0100

    fixed syntax error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -224,6 +224,7 @@ if [ -f "${PJSIP_STASH_PATH}" ]; then
     if [ ! -f "${PJSIP_PATH}" ]; then
         echo "pjsip.conf missing in ${CONFIG_TARGET_DIR}, restoring from image template..."
         REFRESH_PJSIP=true
+    fi
     if [ "${REFRESH_PJSIP}" = true ]; then
         if ! cp -f "${PJSIP_STASH_PATH}" "${PJSIP_PATH}"; then
             echo "ERROR: Failed to refresh pjsip.conf from template at ${PJSIP_STASH_PATH}. Check file permissions and available disk space."
